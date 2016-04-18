acffcc9aa01a6343181ea404c215d9ff3c391171
Wanchaloem Sriwiset <golf.sriwise@hotmail.com>
2016-04-19 00:57:14 +0930

    Adding download and delete file functions

---
+++ CRPEngine/CRPEngine/CRPEngine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRPEngine
+{
+    public partial class CRPEngine : Form
+    {
+        public CRPEngine()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void download_btn_Click(object sender, EventArgs e)
+        {
+            FetchingData myFetchingData = new FetchingData();
+            myFetchingData.downloadData();
+            download_msg.Text = "Successfully Downloaded!!";
+            
+        }
+
+        private void delete_file_Click(object sender, EventArgs e)
+        {
+
+            FileAccess myFileAccess = new FileAccess();
+            string deleteMessage;
+
+            deleteMessage =  myFileAccess.deleteFile();
+
+            download_msg.Text = deleteMessage;
+            
+        }
+    }
+}
---
+++ CRPEngine/CRPEngine/FileAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace CRPEngine
+{
+    class FileAccess
+    {
+
+        private string executableLoaction;
+        private string fileLocation;
+
+        public FileAccess()
+        {
+            executableLoaction = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            fileLocation = Path.Combine(executableLoaction, "resume.pdf");
+        }
+        
+
+        public string deleteFile()
+        {
+            if (File.Exists(fileLocation))
+            {
+                File.Delete(fileLocation);
+                return "The file has been deleted..";
+            }
+            else
+            {
+                return "The file does not exists..";
+            }
+        }
+    }
+}
