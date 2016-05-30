213770fed5d85b4873384f3984c91f28cba643e6
David112 <david.winsalde@live.com>
2016-05-30 17:23:46 +0930

    Revert "Graphing and Cash rate data fetching"
    This reverts commit 7277d7dc9fb5c298a8f9a9f6e3ed220b7f019eb5.
    
    # Conflicts:
    #	CRPEngine/CRPEngine/CRPEngine.Designer.cs
    #	CRPEngine/CRPEngine/CRPEngine.cs
    #	CRPEngine/CRPEngine/CRPEngine.csproj

---
+++ CRPEngine/CRPEngine/CRPEngine.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CRPEngine
+{
+    partial class CRPEngine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.download_msg = new System.Windows.Forms.Label();
+            this.printBtn = new System.Windows.Forms.Button();
+            this.delete_file = new System.Windows.Forms.Button();
+            this.download_btn = new System.Windows.Forms.Button();
+            this.messageTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // download_msg
+            // 
+            this.download_msg.AutoSize = true;
+            this.download_msg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.download_msg.Location = new System.Drawing.Point(23, 9);
+            this.download_msg.Name = "download_msg";
+            this.download_msg.Size = new System.Drawing.Size(9, 20);
+            this.download_msg.TabIndex = 1;
+            this.download_msg.Text = "\r\n";
+            // 
+            // printBtn
+            // 
+            this.printBtn.Location = new System.Drawing.Point(409, 46);
+            this.printBtn.Name = "printBtn";
+            this.printBtn.Size = new System.Drawing.Size(98, 31);
+            this.printBtn.TabIndex = 3;
+            this.printBtn.Text = "Show Data";
+            this.printBtn.UseVisualStyleBackColor = true;
+            this.printBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // delete_file
+            // 
+            this.delete_file.Location = new System.Drawing.Point(409, 83);
+            this.delete_file.Name = "delete_file";
+            this.delete_file.Size = new System.Drawing.Size(98, 31);
+            this.delete_file.TabIndex = 2;
+            this.delete_file.Text = "Delete File";
+            this.delete_file.UseVisualStyleBackColor = true;
+            this.delete_file.Click += new System.EventHandler(this.delete_file_Click);
+            // 
+            // download_btn
+            // 
+            this.download_btn.Location = new System.Drawing.Point(409, 9);
+            this.download_btn.Name = "download_btn";
+            this.download_btn.Size = new System.Drawing.Size(98, 31);
+            this.download_btn.TabIndex = 0;
+            this.download_btn.Text = "Download";
+            this.download_btn.UseVisualStyleBackColor = true;
+            this.download_btn.Click += new System.EventHandler(this.download_btn_Click);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.AcceptsTab = true;
+            this.messageTextBox.Location = new System.Drawing.Point(13, 9);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.ReadOnly = true;
+            this.messageTextBox.Size = new System.Drawing.Size(366, 167);
+            this.messageTextBox.TabIndex = 4;
+            this.messageTextBox.Text = "";
+            // 
+            // CRPEngine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(524, 188);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.delete_file);
+            this.Controls.Add(this.download_btn);
+            this.Controls.Add(this.printBtn);
+            this.Controls.Add(this.download_msg);
+            this.Name = "CRPEngine";
+            this.Text = "CRPEngine";
+            this.Load += new System.EventHandler(this.CRPEngine_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label download_msg;
+        private System.Windows.Forms.Button printBtn;
+        private System.Windows.Forms.Button delete_file;
+        private System.Windows.Forms.Button download_btn;
+        private System.Windows.Forms.RichTextBox messageTextBox;
+    }
+}
+
---
+++ CRPEngine/CRPEngine/CRPEngine.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRPEngine
+{
+    public partial class CRPEngine : Form
+    {
+        public CRPEngine()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void download_btn_Click(object sender, EventArgs e)
+        {
+            FetchingData myFetchingData = new FetchingData();
+            myFetchingData.downloadData();
+            download_msg.Text = "Successfully Downloaded!!";
+            
+        }
+
+        private void delete_file_Click(object sender, EventArgs e)
+        {
+
+            FileAccess myFileAccess = new FileAccess();
+            string deleteMessage;
+
+            deleteMessage =  myFileAccess.deleteFile();
+
+            download_msg.Text = deleteMessage;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[,] message;
+            ExcelReader er = new ExcelReader();
+            message = er.readingExcel();
+
+            for (int column = 0; column < 30; column++)
+            {
+                for(int row = 0; row < 2; row++)
+                {
+                    messageTextBox.Text = messageTextBox.Text + message[column, row] + ("\t\t");
+
+                }
+
+                messageTextBox.Text = messageTextBox.Text + Environment.NewLine;
+
+
+            }
+            
+            
+            
+        }
+
+        private void CRPEngine_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
